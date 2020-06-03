b4b9354e35e687babf88479b352357bc2bfd64b2
nikhilak7 <knikhila752@gmail.com>
2020-06-03 10:34:44 -0700

    Verify usage of var

---
+++ variablesAndConstants/var.js
@@ -0,0 +1,18 @@
+var firstName = 'Nikhila';
+var secondName = 'K';
+
+console.log(firstName +' '+ secondName);
+
+firstName = 'Kannur,';
+firstName = firstName + 'Kerala';
+console.log(firstName);
+
+var firstNumber,
+    secondNumber,
+    sum;
+firstNumber = 14;
+secondNumber = 9;
+sum = firstNumber + secondNumber;
+
+console.log(sum);
+
