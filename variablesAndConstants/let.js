80934008ee1bfb9bf9aa17b8eaaff31e7a1afe0c
nikhilak7 <knikhila752@gmail.com>
2020-06-03 09:52:42 -0700

    Verified reusability

--- variablesAndConstants/let.js
+++ variablesAndConstants/let.js
@@ -2,3 +2,9 @@ let firstName = 'Nikhila ';
 let secondName = 'K';
 
 console.log('Name : ',firstName +' '+ secondName);
+
+firstName = 'Kannur,';
+secondName = 'Kerala';
+
+console.log('Place : ',firstName + secondName);
+
