7d88eb06f72f95c72c099465bcb239d853b59408
nikhilak7 <knikhila752@gmail.com>
2020-06-04 23:47:38 -0700

    Verify round, power and square root

---
+++ dataTypes/mathObject.js
@@ -0,0 +1,22 @@
+let numberOne = 32,
+  numberTwo = 64,
+  numberThree = -45,
+  numberFour = 34.66,
+  numberFive = 45.342;
+
+console.log(
+  "Nearest integer of " + numberFour + " : " + Math.round(numberFour)
+);
+console.log("_____________________________________________");
+
+console.log(
+  numberTwo +
+    "th power of " +
+    numberOne +
+    " is " +
+    Math.pow(numberOne, numberTwo)
+);
+console.log("_____________________________________________");
+
+console.log("Square root of " + numberTwo + " is " + Math.sqrt(numberTwo));
+console.log("_____________________________________________");
