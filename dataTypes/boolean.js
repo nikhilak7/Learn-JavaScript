3fc644ba68e3ef2af0be28f55c932df35f537ec0
nikhilak7 <knikhila752@gmail.com>
2020-06-05 08:43:58 -0700

    Add method compareNumberAndPerformOperation

--- dataTypes/boolean.js
+++ dataTypes/boolean.js
@@ -13,3 +13,19 @@ checkBooleanValue(9.99);
 checkBooleanValue(-8.456);
 checkBooleanValue(null);
 
+function compareNumberAndPerformOperation(firstNumber, secondNumber) {
+    console .log("____________________________________________");
+    console.log("The numbers are " + firstNumber +" , "+ secondNumber);
+      if (firstNumber > secondNumber) {
+          console.log("Difference = ", firstNumber - secondNumber);
+        } 
+        else if (firstNumber<secondNumber) {
+            console.log("Sum = ", firstNumber + secondNumber);
+        }
+        else console.log("Sqrt = ", Math.sqrt(firstNumber) );
+      
+  }
+  
+  compareNumberAndPerformOperation(96,78);
+  compareNumberAndPerformOperation(43,67);
+  compareNumberAndPerformOperation(7,7);
