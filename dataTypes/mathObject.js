5f9b425782d2c7ce8bfff2cb1a445530764b854b
nikhilak7 <knikhila752@gmail.com>
2020-06-05 01:44:11 -0700

    Verify absolute, sine, cosine, minimum and maximum

--- dataTypes/mathObject.js
+++ dataTypes/mathObject.js
@@ -20,3 +20,22 @@ console.log("_____________________________________________");
 
 console.log("Square root of " + numberTwo + " is " + Math.sqrt(numberTwo));
 console.log("_____________________________________________");
+
+console.log("Absolute value of " + numberThree + " is " + Math.abs(numberThree));
+console.log("_____________________________________________");
+
+console.log("Highest nearest integer of " + numberFive + " is " + Math.ceil(numberFive));
+console.log("_____________________________________________");
+
+console.log("Sine of angle " + numberOne + " is " + Math.sin(numberOne * Math.PI / 180));
+console.log("_____________________________________________");
+
+console.log("Cosine of angle " + numberOne + " is " + Math.cos(numberOne * Math.PI / 180));
+console.log("_____________________________________________");
+
+console.log("Smallest number is " + Math.min(numberOne,numberTwo,numberThree,numberFour,numberFive));
+console.log("_____________________________________________");
+
+console.log("Largest number is " + Math.max(numberOne,numberTwo,numberThree,numberFour,numberFive));
+console.log("_____________________________________________");
+
