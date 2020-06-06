4902b2584ff4b6488392f9b3013824715d5b95ec
nikhilak7 <knikhila752@gmail.com>
2020-06-06 12:14:18 -0700

    Add method addItemToStack

--- dataTypes/stackAndQueue.js
+++ dataTypes/stackAndQueue.js
@@ -20,3 +20,15 @@ console.log(randomQueue);
 deleteItemFromQueue(randomQueue);
 console.log(randomQueue);
 
+addItemToStack = (stack, element) => {
+    let sizeOfStack = stack.length;
+    for (let index = sizeOfStack; index >= 1; index--) {
+        stack[index] = stack[index-1];
+    }
+    stack[0] = element;
+}
+
+addItemToStack(randomStack, 8);
+addItemToStack(randomStack, 3);
+addItemToStack(randomStack, 5);
+console.log(randomStack);
